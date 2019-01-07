9bbe0641e97e1b2d39b60c29e73029617aea4901
rizkisadewa <rizkykysadewa@gmail.com>
2019-01-07 14:57:27 +0800

    still on progress register for admin

--- project/models/adminModels.py
+++ project/models/adminModels.py
@@ -1,11 +1,2 @@
 # -*- coding: utf-8 -*-
 from flask import flash
-
-app = Flask(__name__)
-
-# Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'myAkar01'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
